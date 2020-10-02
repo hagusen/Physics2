de500766f818825961f9f3903c24cdb1c5f27865
hagusen <john@bit-ab.com>
2020-10-03 00:57:09 +0200

    Calculated masses

--- Assets/Scenes/Planet.cs
+++ Assets/Scenes/Planet.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class Planet : MonoBehaviour
 {
 
+
+    // We scale down the radius of the planets by 10^6
+    // Then we can calculate the new mass with this thing: (4/3)*("insert group name")* radius^3 * density  
+
+
+
+
     public float mass;
     public float radius;
+    public float density; // g/cm^3
 
 
 
 
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //dsfds
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    private void OnValidate() {
+        transform.localScale = Vector3.one * radius;
+
+        //note g/cm^3 -> kg/m^3 density 
+        mass = (4 / 3) * Mathf.PI * Mathf.Pow(radius, 3) * density * 1000;
     }
 }
