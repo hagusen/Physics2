e6be9b53f20e981b8e76fb3961d23c4b90939c70
hagusen <john@bit-ab.com>
2020-10-02 21:50:48 +0200

    no message

---
+++ Assets/Scenes/Planet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+
+    public float mass;
+    public float radius;
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //dsfds
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
