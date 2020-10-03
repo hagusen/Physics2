357c9f7d4c77110544d2ea34fa4e6d390ae3d499
hagusen <john@bit-ab.com>
2020-10-04 01:58:27 +0200

    other stuff

---
+++ Assets/Scenes/other/ParticleCollision.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.ParticleSystemJobs;
+
+ [ExecuteInEditMode]
+public class ParticleCollision : MonoBehaviour
+{
+
+    ParticleSystem ps;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ParticleSystem.Particle[] p = new ParticleSystem.Particle[ps.particleCount];
+        ps.GetParticles(p);
+
+
+        for (int i = 0; i < p.Length; i++) {
+            for (int k = 0; k < p.Length; k++) {
+
+
+
+                float dist = Vector3.Distance(p[i].position, p[k].position) - (p[i].GetCurrentSize(ps) + p[k].GetCurrentSize(ps));
+
+                if (0 > dist) {
+
+                    p[i].position = p[i].position + Vector3.up * p[i].GetCurrentSize(ps) * p[k].GetCurrentSize(ps);
+
+
+                }
+            }
+        }
+
+        ps.SetParticles(p);
+    }
+}
