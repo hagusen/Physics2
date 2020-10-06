20d74ed7a93d9f9d531cd4f67a9ad6872767e7a0
hagusen <john@bit-ab.com>
2020-10-06 23:52:26 +0200

    almost final Update

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -1,88 +1,114 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Diagnostics;
+using UnityEngine.SceneManagement;
 
+
+//
+// Av: John Boman, Martin Quach
+// c18johbo, a18marqu
 public class Controller : MonoBehaviour
 {
 
     public Planet[] planets;
-    float physicsUpdateTimer = 0;
     public float timescale = 1;
 
+    float physicsUpdateTimer = 0;
 
-    //Other
+    bool gravityToggle = true;
+    float gravityMultiplier = 1;
+
+    float density;
+
+    ////Other
     private ParticleObject[] pObjs;
-
-
     void Start()
     {
         pObjs = new ParticleObject[planets.Length];
         for (int i = 0; i < planets.Length; i++) {
             pObjs[i] = planets[i].GetComponent<ParticleObject>();
         }
+
+        density = planets[0].density;
     }
 
-    // Update is called once per frame
+    [System.Obsolete]
     void Update() {
-        Time.timeScale = timescale;
 
+
+        // Turn off gravity
+        if (Input.GetKeyDown(KeyCode.Space))
+            gravityToggle = !gravityToggle;
+
+        //Mult gravity
+        if (Input.GetKeyDown(KeyCode.J)) 
+            gravityMultiplier *= 2;
+
+        //Divide gravity
+        if (Input.GetKeyDown(KeyCode.H)) 
+            gravityMultiplier /= 2;
+
+        //Update density
+        planets[0].SetDensity(density * gravityMultiplier);
+
+        //Reload Scene
+        if (Input.GetKeyDown(KeyCode.T))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        // Turn off the program lul (Will crash unity also)
+        if (Input.GetKey(KeyCode.P) && Input.GetKey(KeyCode.O)&& Input.GetKey(KeyCode.I))
+            Application.ForceCrash(0);
+
+        //Change TimeScale in editor
+        Time.timeScale = timescale;
 
         //Fixed Physics update timer
         physicsUpdateTimer += Time.deltaTime;
         if (physicsUpdateTimer > Constants.STEPVALUE) {
             physicsUpdateTimer -= Constants.STEPVALUE; // :/
 
+            if (gravityToggle) {
 
-            foreach (var planet in planets) {
+                // Calculate the force for all planets (moon and earth)
+                foreach (var planet in planets) {
 
-                planet.CalculateForce(planets, Constants.STEPVALUE);
+                    planet.CalculateForce(planets, Constants.STEPVALUE);
+                }
+
+                // Apply the force for all planets (moon and earth)
+                foreach (var planet in planets) {
+
+                    planet.UpdatePositon(Constants.STEPVALUE);
+                }
             }
 
-            //Update graphic
-            foreach (var planet in planets) {
-
-                planet.UpdatePositon(Constants.STEPVALUE);
-            }
-
-
-
+            //Other
             // Calculate the collisions for the particles
             foreach (var obj in pObjs) {
                 obj.CalculateCollisions(planets, Constants.STEPVALUE);
             }
-
-
-
         }
     }
+
     [Header("Calculations")]
-        public float orbitalVelocity;
-        public float orbitalVelocity1;
+    public float orbitalVelocity;
+    public float orbitalVelocity1;
 
     void OnValidate() {
 
-
+        //Get Distance from this planet to the other
         Vector3 distVector = planets[0].transform.position - planets[1].transform.position;
         float distSquared = Vector3.SqrMagnitude(distVector); ;
 
-        // F = G*((m1*m2)/r^2)
-        float forceMagnitude = Constants.G * ((planets[1].mass * planets[0].mass) / distSquared) * 10000000;
-        Vector3 force = distVector.normalized * forceMagnitude;
-
-        //get acceleration from force (we could just removed mass above but meh)
-        Vector3 acceleration = force / planets[1].mass;
-
-
+        // Calculate the orbit velocity for the earth and moon (The velocity needed to orbit the other planet)
         //http://www.phys.ufl.edu/courses/phy2048/archives/fall06/lectures/11-08EscapeVelocity.pdf
-        orbitalVelocity = Mathf.Sqrt((Constants.G * planets[0].mass) / Mathf.Sqrt(distSquared));
-        orbitalVelocity1 = Mathf.Sqrt((Constants.G * planets[1].mass) / Mathf.Sqrt(distSquared));
+        orbitalVelocity = Mathf.Sqrt((Constants.G * planets[0].mass * 10000000) / Mathf.Sqrt(distSquared));
+        orbitalVelocity1 = Mathf.Sqrt((Constants.G * planets[1].mass * 10000000) / Mathf.Sqrt(distSquared));
 
 
-        // bättre kamera
-        // interaktivt!
-        // stäng av gravitions kraften
-        // stäng av månens velocity?
-        // öka gravitions kraften?
+
+
     }
 
 
--- Assets/Planet.cs
+++ Assets/Planet.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//
+// Av: John Boman, Martin Quach
+// c18johbo, a18marqu
 public class Planet : MonoBehaviour
 {
 
 
-    // We scale down the radius of the planets by 10^6
+    // We scale down the radius of the planets by 10^6 (m)
     // Then we can calculate the new mass with this thing: (4/3)*("insert group name")* radius^3 * density
-    // based on the density of the planet
+    // based on the density of each planet
 
 
 
@@ -19,50 +22,51 @@ public class Planet : MonoBehaviour
     public Vector3 curVelocity;
 
 
-    public float orbitalSpeed;
-
+    //Calculate all forces affecting this planet 
     public void CalculateForce(Planet[] planets, float timeStep) {
         foreach (var planet in planets) {
             if (planet != this) {
 
-                
 
+                //Get Distance from this planet to the other
                 Vector3 distVector = planet.transform.position - transform.position;
-                float distSquared = Vector3.SqrMagnitude(distVector); ;
+                float distSquared = Vector3.SqrMagnitude(distVector);
 
+
+                // The gravition law scaled by 10^7 so it's not so slow...
                 // F = G*((m1*m2)/r^2)
                 float forceMagnitude = Constants.G * ((this.mass * planet.mass) / distSquared) * 10000000;
                 Vector3 force = distVector.normalized * forceMagnitude;
 
-                //get acceleration from force (we could just removed mass above but meh)
+                //get acceleration from force (we could have just have removed it before, above but meh)
                 Vector3 acceleration = force / mass;
+
                 //add acceleration to current Velocity
                 curVelocity += acceleration * timeStep;
-
-                //http://www.phys.ufl.edu/courses/phy2048/archives/fall06/lectures/11-08EscapeVelocity.pdf
-                orbitalSpeed = Mathf.Sqrt((Constants.G * planet.mass * 10000000) / Mathf.Sqrt(distSquared));
 
             }
         }
     }
 
 
-
+    //Update Positon of this planet
     public void UpdatePositon(float timeStep) {
-
         transform.position += curVelocity * timeStep;
     }
 
 
+    public void SetDensity(float density) {
+        this.density = density;
 
+        //Calculate mass from the radius
+        //note g/cm^3 -> kg/m^3 density (hence * 1000)
+        mass = (4 / 3) * Mathf.PI * Mathf.Pow(radius, 3) * density * 1000;
+    }
 
 
     private void OnValidate() {
         transform.localScale = Vector3.one * radius;
 
-        
-
-        //note g/cm^3 -> kg/m^3 density 
-        mass = (4 / 3) * Mathf.PI * Mathf.Pow(radius, 3) * density * 1000;
+        SetDensity(density);
     }
 }
